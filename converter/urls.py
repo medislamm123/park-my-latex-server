7cef2b43a36a58c3a6033f6ff409cc4755170bd0
mekadmini <m.mekadmini@campus.lmu.de>
2024-03-17 14:11:36 +0100

    Co-authored-by: gehasel <gehasel@users.noreply.github.com>

--- converter/urls.py
+++ converter/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.upload_page, name='upload_page'),
     path('upload/', views.convert_file, name='convert_file'),
     path('download/', views.download_view, name='download_view'),
     path('uploadpage/', views.upload_page, name='upload_page'),
--- parkmylatexserver/urls.py
+++ parkmylatexserver/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = (
     [
         path("admin/", admin.site.urls),
-        path("converter/", include("converter.urls")),  # Adjust 'converter' as needed
+        path("", include("converter.urls")),  # Adjust 'converter' as needed
     ]
     #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
